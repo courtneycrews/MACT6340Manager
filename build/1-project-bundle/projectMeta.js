45802d09f7054d61bc70abaee033c2d6d5c7d32c
courtneycrewsu <crews@smu.edu>
2024-08-07 23:26:44 -0700

    Completed front end part 1

---
+++ build/1-project-bundle/projectMeta.js
@@ -0,0 +1,21 @@
+// mySQL is sensitive - ensure you pay close attention to quotes and formats and datatypes
+export const projectInfo = {
+  projectName: "Icosahedron Geometry",
+  artistsPayoutWallet: "0x94848CEe6eA7dBcc5322f0B13015A42ec63bC3BB",
+  numberOfEditions: 13,
+  releaseDate: "2024-01-26 09:00:00",
+  websiteProjectDescription:
+    "A single, gorgeous icosahedron rotates around an axis.  The background color and color of the shape are determined by each unique hash.",
+  price: 0.01,
+  royaltiesPercent: 5.0,
+  titleForViewport: "An Icosahedron Geometry in THREEjs",
+  projectWebUrl: "https://www.dgs-creative.com", // full url as shown
+  openSeaCollectionName: "Icosahedron Geometry",
+  openSeaCollectionDescription:
+    "A Collection of beautiful Icosahedron interactive shapes rendered with THREEjs.",
+  openSeaCollectionImage: "",
+  openSeaCollectionSeller_fee_basis_points: 500,
+  openSeaCollectionFee_recipient: "0x94848CEe6eA7dBcc5322f0B13015A42ec63bC3BB",
+  tokenDescriptionText:
+    "One of the gorgeous interactive shapes from the Icosahedron Geometry collection.",
+};
